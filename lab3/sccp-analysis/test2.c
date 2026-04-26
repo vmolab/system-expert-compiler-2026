b9b4916932e4b0da2ae605a8ee5550e5f3809c81
Donguk Ju <donguk.red@snu.ac.kr>
2026-04-26 16:22:26 +0000

    add test2

---
+++ lab3/sccp-analysis/test2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    int x = 4;
+
+    if (x < 5)
+    {
+        a = 1;
+    }
+    else
+    {
+        a = 2;
+    }
+
+    printf("%d\n", a);
+
+    return 0;
+}
